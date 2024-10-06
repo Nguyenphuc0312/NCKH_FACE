150c58e1fd6f8b2493e35400688b352d38d16c62
Nguyenphuc0312 <nxp1212005@gmail.com>
2024-10-06 22:24:15 +0700

    Them allow camera

--- testv5.py
+++ testv5.py
@@ -24,6 +24,7 @@ def gen_frames():
 def index():
     return render_template('index.html')
 
+
 @app.route('/video_feed')
 def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
